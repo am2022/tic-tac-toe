b5f7fe4a72abbda3b1ae7a42d39d28408ea7bdd0
am <am2022a2@gmail.com>
2022-11-16 16:35:06 +0330

    add check win system for btn3

--- tic_tac_toe/tic_tac_toe/Form1.cs
+++ tic_tac_toe/tic_tac_toe/Form1.cs
@@ -108,6 +108,16 @@ namespace tic_tac_toe
                 btn3.Text = "X";
                 btn3.Enabled = false;
                 this.game_board[2] = 'x';
+
+                this.get_win = this.cw.is_winner(this.game_board, turn);
+                if (this.get_win == 'x')
+                {
+                    MessageBox.Show("x wins!");
+                    this.dump = lb_pls1.Text;
+                    this.dumpi = int.Parse(this.dump);
+                    dumpi++;
+                    lb_pls1.Text = dumpi.ToString();
+                }
             }
             else
             {
@@ -115,6 +125,16 @@ namespace tic_tac_toe
                 btn3.Text = "O";
                 btn3.Enabled = false;
                 this.game_board[2] = 'o';
+
+                this.get_win = this.cw.is_winner(this.game_board, turn);
+                if (this.get_win == 'o')
+                {
+                    MessageBox.Show("o wins!");
+                    this.dump = lb_pls2.Text;
+                    this.dumpi = int.Parse(this.dump);
+                    dumpi++;
+                    lb_pls2.Text = dumpi.ToString();
+                }
             }
 
             this.turn++;
