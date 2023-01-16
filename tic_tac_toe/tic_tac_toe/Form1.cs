9bce424284634e71401156911c1e2efbfc58e2bf
am <am2022a2@gmail.com>
2023-01-16 19:13:31 +0330

    check draw for btn9

--- tic_tac_toe/tic_tac_toe/Form1.cs
+++ tic_tac_toe/tic_tac_toe/Form1.cs
@@ -669,6 +669,22 @@ namespace tic_tac_toe
                     lb_pls1.Text = dumpi.ToString();
                     this.make();
                 }
+                else if (this.get_win == 'd')
+                {
+                    MessageBox.Show("Draw!");
+
+                    this.dump = lb_pls1.Text;
+                    this.dumpi = int.Parse(this.dump);
+                    dumpi++;
+                    lb_pls1.Text = dumpi.ToString();
+
+                    this.dump = lb_pls2.Text;
+                    this.dumpi = int.Parse(this.dump);
+                    dumpi++;
+                    lb_pls2.Text = dumpi.ToString();
+
+                    this.make();
+                }
             }
             else
             {
@@ -685,6 +701,22 @@ namespace tic_tac_toe
                     this.dumpi = int.Parse(this.dump);
                     dumpi++;
                     lb_pls2.Text = dumpi.ToString();
+                    this.make();
+                }
+                else if (this.get_win == 'd')
+                {
+                    MessageBox.Show("Draw!");
+
+                    this.dump = lb_pls1.Text;
+                    this.dumpi = int.Parse(this.dump);
+                    dumpi++;
+                    lb_pls1.Text = dumpi.ToString();
+
+                    this.dump = lb_pls2.Text;
+                    this.dumpi = int.Parse(this.dump);
+                    dumpi++;
+                    lb_pls2.Text = dumpi.ToString();
+
                     this.make();
                 }
             }
